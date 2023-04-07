e2d8daf297f4299032c5e303fae819e96c0d53eb
Surendhar <studyspace.ie@gmail.com>
2023-04-07 17:09:05 +0530

    Starting project  section styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import {Routes,Route} from 'react-router-dom';
-import Projects from './pages/projects';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Skills from './pages/Skills';
 import sea from '../src/images/sea.jpg';
 import countryroad from '../src/images/countryroad.jpg';
-import empire from '../src/images/empire.jpg';
-import castle from '../src/images/castle.jpg';
+
 
 
 
@@ -18,7 +19,9 @@ function App() {
         <Navbar/>
         <Routes>
             <Route path='/' element={<Main/>}></Route>
+            <Route path='/About' element={<About/>}></Route>
             <Route path='/projects' element={<Projects/>}></Route>
+            <Route path='/Skills' element={<Skills/>}></Route>
         </Routes>
     <img src={countryroad} alt='sea' className='absolute top-0 right-0 -z-10 h-screen w-screen'></img>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 const Navbar = () => {
   return (
     <div className='lg:flex lg:flex-col'>
+        
         <ul className='flex gap-10 font-melodrama text-black justify-center items-center translate-y-10 '>
         <li className='text-2xl hover:blur-none transition ease-in-out duration-300 hover:scale-x-150 hover:scale-y-150 hover:font-medium'>
-            <Link>ABOUT</Link>
+            <Link to='/About'>ABOUT</Link>
         </li>
 
         <li className='text-2xl hover:blur-none transition ease-in-out duration-300 hover:scale-x-150 hover:scale-y-150 hover:font-medium'>
-             <Link>PROJECTS</Link>
+             <Link key='project' to='/projects'>PROJECTS</Link>
         </li>
         
         <li>
-             <Link className='text-white text-4xl blur-xs'>SurendharGN</Link>
+             <Link to='/' className='text-white text-4xl blur-xs  hover:blur-none transition ease-in-out duration-300 hover:scale-x-150 hover:scale-y-150 hover:font-medium'>SurendharGN</Link>
         </li>
         
         <li className='text-2xl hover:blur-none transition ease-in-out duration-300 hover:scale-x-150 hover:scale-y-150 hover:font-medium'>
