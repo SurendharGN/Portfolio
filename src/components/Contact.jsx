885640f39bae69b48b686e2cfda0798773af62f7
Surendhar <studyspace.ie@gmail.com>
2023-04-10 18:37:44 +0530

    finished contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,13 +8,15 @@ const Contact = () => {
     <div className='flex gap-3 justify-center mobile:translate-y-5 lg:absolute lg:top-[34rem] lg:translate-y-0'>
             
         <div className='border rounded-full w-10 '>
-            <img className='w-10 p-2' src= {linkedin} alt='linkedin-logo'></img>
+            <Link to='https://www.linkedin.com/in/surendhar-gn-28a61a270/' target="_blank"><img className='w-10 p-2' src= {linkedin} alt='linkedin-logo'></img></Link>
+            
         </div>
         
-        <Link to='/' className='border border-white px-4 rounded-full p-2'>CONTACT ME</Link>
+        <Link to='mailto:work.surendhar@gmail.com' className='border border-white px-4 rounded-full p-2'>CONTACT ME</Link>
 
         <div className='border rounded-full w-10 '>
-            <img className='w-10 ' src= {github} alt='github-logo'></img>
+            <Link to='https://github.com/SurendharGN' target='_blank'><img className='w-10 ' src= {github} alt='github-logo'></img></Link>
+            
         </div>
         </div>
   )
