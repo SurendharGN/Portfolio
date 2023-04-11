38d21db0d7bbb955782583b18c9ec5513f987aed
Surendhar <studyspace.ie@gmail.com>
2023-04-11 22:05:56 +0530

    added title

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Resume = () => {
   return (
-     <iframe title='Resume' src="https://drive.google.com/file/d/1Rfo8iP8McvmCqnpb9ud-cSHZ9_jOY5Wp/preview" width="100%" height='768px' allow="autoplay"></iframe>
+    <iframe title="Resume" src="https://drive.google.com/file/d/1Rfo8iP8McvmCqnpb9ud-cSHZ9_jOY5Wp/preview" width="100%" height="770" allow="autoplay"></iframe>
   )
 }
 
