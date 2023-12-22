555ccae979cfac947941878cde27d4e4eb0db66c
Surendhar <studyspace.ie@gmail.com>
2023-12-22 21:27:07 +0530

    getting started with building the prototype.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <div>
             <header id="header">
                 <h1 id="desc">Surendhar GN <br></br>Web Enthusiast based in India.</h1>
+                <img id='pfp' src=''></img>
             </header>
             <main id="main">
                 <p id="about">Sophomore majoring in Artificial Intelligence & Machine Learning.
