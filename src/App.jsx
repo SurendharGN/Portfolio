6e97699e91df80875b56010deb393c9c57aaaee7
SurendharGN <work.surendhar@gmail.com>
2024-02-05 23:04:45 +0530

    staging a minimaistic design approach.

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,69 @@ import { useState } from 'react'
 
 import './App.css'
 
+import { Link } from 'react-router-dom'
+
 function App() {
 
 
   return (
-    <>
-        <div>
-            <header id="header">
-                {/* <h1 id="desc">Surendhar GN <br></br>Web Enthusiast based in India.</h1> */}
-                <img id='pfp' src=''></img>
-            </header>
-            <main id="main">
-                <p id="about">
+    <div>
+        <header>
+          <nav id='navbar'>
+            <div >
+              <Link id='gn.s'>GN.S</Link>
+              <img id='theme-selector'></img>
+            </div>
+            <ul id='links-container'>
+              <Link className='nav-link'>Home.</Link>
+              <Link className='nav-link'>About.</Link>
+              <Link className='nav-link'>Links.</Link>
+              <Link className='nav-link'>Contact.</Link>
+            </ul>
+          </nav>
 
-The portfolio is a work in progress and requires a month to be published.
+        </header>
 
-</p>
-            </main>
-            <footer id="footer"></footer>
-            
-        </div>
-    </>
+        <main>
+
+          <h1>
+            SURENDHAR GN.
+            <span id='highlighter'></span>
+          </h1>
+
+          <div>
+            <p>Designer-</p>
+            <p>Developer</p>
+          </div>
+
+          <span id='separator'></span>
+
+          <p>A geek based in <span id='india'>India</span>, willing to <Link className='underline-para'>freelance</Link> my career and buils a reputation around <Link className='underline-para'>FOSS</Link>.</p>
+
+          <img id='hero-image'></img>
+
+          <div id='project-section'>
+            <div>
+              <h3 id='project'>Projects.</h3>
+              <p id='project-desc'>A curated list of work done as a part of my resume.</p>
+            </div>
+
+            <div>
+              <h3 id='playground'>Playground.</h3>
+              <p id='playground-desc'>A list of work done under my procrastinated ideas.</p>
+
+            </div>
+
+          </div>
+
+
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </div>
   )
 }
 
