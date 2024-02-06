2aee4f94c5d61b0ef99387271166e413c36875a2
SurendharGN <work.surendhar@gmail.com>
2024-02-06 11:11:56 +0530

    homepage designing

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,36 @@ function App() {
 
 
   return (
-    <div>
+    <div id='main'>
+
+      <div id='left'>
+        <Link id='gns'>GN.S</Link>
+        <h1>
+            SURENDHAR GN.
+            <span id='highlighter'></span>
+          </h1>
+
+          <div id='title'>
+            <p>Designer-</p>
+            <p>Developer</p>
+          </div>
+
+          <div id='separator'></div>
+          
+
+          <p id='description'>A geek based in <span id='india'>India</span>, willing to <Link className='underline-para'>freelance</Link> my career and build a reputation around <Link className='underline-para'>FOSS</Link>.</p>
+      </div>
+
+      <div id='center'>
+        
+      </div>
+      
+
+      <div id='right'>
+        <img id='hero-image' src='../src/assets/homepage.png'></img>
         <header>
           <nav id='navbar'>
             <div >
-              <Link id='gn.s'>GN.S</Link>
               <img id='theme-selector'></img>
             </div>
             <ul id='links-container'>
@@ -22,48 +47,26 @@ function App() {
               <Link className='nav-link'>Contact.</Link>
             </ul>
           </nav>
-
         </header>
-
-        <main>
-
-          <h1>
-            SURENDHAR GN.
-            <span id='highlighter'></span>
-          </h1>
-
-          <div>
-            <p>Designer-</p>
-            <p>Developer</p>
-          </div>
-
-          <span id='separator'></span>
-
-          <p>A geek based in <span id='india'>India</span>, willing to <Link className='underline-para'>freelance</Link> my career and buils a reputation around <Link className='underline-para'>FOSS</Link>.</p>
-
-          <img id='hero-image'></img>
-
-          <div id='project-section'>
-            <div>
-              <h3 id='project'>Projects.</h3>
+        <div id='project-section'>
+            <div id='projects'>
+              <h3 id='project-title'>Projects.</h3>
               <p id='project-desc'>A curated list of work done as a part of my resume.</p>
             </div>
 
-            <div>
-              <h3 id='playground'>Playground.</h3>
+            <div id='separator'></div>
+
+            <div id='playground'>
+              <h3 id='playground-title'>Playground.</h3>
               <p id='playground-desc'>A list of work done under my procrastinated ideas.</p>
 
             </div>
 
           </div>
+      </div>
+        
 
-
-
-        </main>
-
-        <footer>
-
-        </footer>
+        
     </div>
   )
 }
